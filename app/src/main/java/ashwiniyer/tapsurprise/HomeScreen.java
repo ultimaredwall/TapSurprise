5b333290547fb5c5665763ff295e4d6dd442617b
Ashwin <ashwinscool@gmail.com>
2015-09-15 19:52:06 -0400

    Created the beginning screen

--- app/src/main/java/ashwiniyer/tapsurprise/HomeScreen.java
+++ app/src/main/java/ashwiniyer/tapsurprise/HomeScreen.java
@@ -1,9 +1,10 @@
 package ashwiniyer.tapsurprise;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -11,27 +12,24 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        //When any part of the screen is tapped, we take the user to the next screen/activity (i.e. the main activity)
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.layout);
+        rl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMain();
+            }
+        });
+
+
+    }
+    private void startMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
-}
